76a75da5bc64e32a4ca8e96f6f952fb6e54338b8
Plewr32B <aliefek2016@gmail.com>
2024-01-22 15:51:47 +0300

    Level 2 Bugfix

--- Scripts/Level2Portal.gd
+++ Scripts/Level2Portal.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
-func _on_Level1Portal_body_entered(body):
+func _on_Level2Portal_body_entered(body):
 	if body.name == "Player":
 		get_tree().change_scene("res://Scenes/Level3.tscn")
--- Scripts/LevelDevam2.gd
+++ Scripts/LevelDevam2.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+func _ready():
+		$Level2Portal.visible = false
+		$Level2Portal.monitoring = false
+
 func _process(_delta):
 	if Global.Level2 == true:
-		$Level1Portal.visible = true
-		$Level1Portal.monitoring = true
+		$Level2Portal.visible = true
+		$Level2Portal.monitoring = true
